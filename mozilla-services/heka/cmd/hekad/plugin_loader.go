48664c727e74326b828d6cdaec595be79c61d057
kennyzhu <2202082952@qq.com>
2017-03-21 10:39:10 +0800

    提交golang版本logstash代码

---
+++ mozilla-services/heka/cmd/hekad/plugin_loader.go
@@ -0,0 +1,7 @@
+package main
+
+import (
+	 _ "github.com/mozilla-services/heka-mozsvc-plugins"
+	 _ "github.com/mozilla-services/heka/plugins/docker"
+	 _ "github.com/mozilla-services/heka/sandbox/plugins"
+)
